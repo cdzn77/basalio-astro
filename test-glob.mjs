acc8f0e14490d99e11cfe59e6a561b32723525ca
Angelo Manzano Jr <chrowmdesigns@gmail.com>
2026-08-09 16:43:17 -0400

    Clean up diagnostic scripts

--- test-glob.mjs
+++
@@ -1,8 +0,0 @@
-const icons = import.meta.glob('/public/assets/icons/blocks/*.svg', {
-  as: 'url', eager: true
-});
-
-console.log('import.meta.glob with as: url returns:');
-console.log('First key:', Object.keys(icons)[0]);
-console.log('First value type:', typeof Object.values(icons)[0]);
-console.log('First value:', Object.values(icons)[0]);
